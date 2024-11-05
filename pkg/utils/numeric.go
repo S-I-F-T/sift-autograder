a10452bb2c76313e2073ab8f7bfff5876c250d21
huysteve26 <huynhs@rpi.edu>
2024-11-05 12:08:06 -0500

    documentation for ContainsAll() & IsUnit() in utils folder.

--- pkg/utils/numeric.go
+++ pkg/utils/numeric.go
@@ -1,6 +1,9 @@
 // Package utils provides general utility functionality.
 package utils
 
+/* Checks whether nums is approximately 
+within the unit interval [0,1] but with
+a tolerance threshold. */
 func IsUnit(num, threshold float64) bool {
 	return num >= -threshold && num <= 1+threshold
 }
