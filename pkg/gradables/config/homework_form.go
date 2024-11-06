2dfa50c9daac0750276daaad8b9e3fec2fd8e3ef
huysteve26 <156709309+huysteve26@users.noreply.github.com>
2024-11-06 05:25:01 -0500

    added some documentation for homework_form.go

--- pkg/gradables/config/homework_form.go
+++ pkg/gradables/config/homework_form.go
@@ -3,10 +3,9 @@ package gradables
 /* Constructing Homework struct from homework_form.json
 https://github.com/Submitty/Submitty/tree/main/autograder/tests/data */
 type Homework struct {
-	GradableID string `json:"gradable_id"`
-	ConfigPath string `json:"config_path"`
-	DateDue string `json:"date_due"`
-	UploadType string `json:upload_type`
-	Subdirectory string `json:subdirectory`
-	VCSPartialPath string `json:vcs_partial_path`
+	GradableID string `json:"gradable_id"` // id for hw gradeable
+	ConfigPath string `json:"config_path"` // configuration path for homework
+	DateDue string `json:"date_due"` // due date for assignment
+	UploadType string `json:upload_type` // upload types for the homework
+	Subdirectory string `json:subdirectory` // hw subdirectories
 }
