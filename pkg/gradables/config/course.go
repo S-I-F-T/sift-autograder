e2b7044d9e8b2c34023f26e34b0dca5d7cf62476
huysteve26 <156709309+huysteve26@users.noreply.github.com>
2024-11-06 05:10:13 -0500

    just initializing course.go for later

---
+++ pkg/gradables/config/course.go
@@ -0,0 +1,7 @@
+package gradables
+
+/* Constructing Course struct from config.json
+https://github.com/Submitty/Submitty/blob/main/autograder/tests/data/config_files */
+type Course struct {
+	// TODO
+}
