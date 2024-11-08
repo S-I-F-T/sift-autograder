dac8259fb942d161b7bf7a52b01ddfc81a8b29ac
huysteve26 <huynhs@rpi.edu>
2024-11-08 16:42:05 -0500

    course.go and homework_form.go in go format

--- pkg/gradables/config/homework_form.go
+++ pkg/gradables/config/homework_form.go
@@ -1,11 +1,14 @@
 package gradables
 
-/* Constructing Homework struct from homework_form.json
-https://github.com/Submitty/Submitty/tree/main/autograder/tests/data */
+/*
+	Constructing Homework struct from homework_form.json
+
+https://github.com/Submitty/Submitty/tree/main/autograder/tests/data
+*/
 type Homework struct {
-	GradableID string `json:"gradable_id"` // id for hw gradeable
-	ConfigPath string `json:"config_path"` // configuration path for homework
-	DateDue string `json:"date_due"` // due date for assignment
-	UploadType string `json:"upload_type"` // upload types for the homework
+	GradableID   string `json:"gradable_id"`  // id for hw gradeable
+	ConfigPath   string `json:"config_path"`  // configuration path for homework
+	DateDue      string `json:"date_due"`     // due date for assignment
+	UploadType   string `json:"upload_type"`  // upload types for the homework
 	Subdirectory string `json:"subdirectory"` // hw subdirectories
 }
