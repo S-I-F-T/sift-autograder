77f520aa67bcab0e6e7c7be4ae7cdbae78a18301
huysteve26 <huynhs@rpi.edu>
2024-11-08 16:37:29 -0500

    fixed type incompatability in homework_form.go

--- pkg/gradables/config/homework_form.go
+++ pkg/gradables/config/homework_form.go
@@ -6,6 +6,6 @@ type Homework struct {
 	GradableID string `json:"gradable_id"` // id for hw gradeable
 	ConfigPath string `json:"config_path"` // configuration path for homework
 	DateDue string `json:"date_due"` // due date for assignment
-	UploadType string `json:upload_type` // upload types for the homework
-	Subdirectory string `json:subdirectory` // hw subdirectories
+	UploadType string `json:"upload_type"` // upload types for the homework
+	Subdirectory string `json:"subdirectory"` // hw subdirectories
 }
