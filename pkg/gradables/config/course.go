fb584139f2462a0511bf1c957aaf2362fff6f045
huysteve26 <huynhs@rpi.edu>
2024-11-08 16:36:32 -0500

    worked on course struct

--- pkg/gradables/config/course.go
+++ pkg/gradables/config/course.go
@@ -3,5 +3,13 @@ package gradables
 /* Constructing Course struct from config.json
 https://github.com/Submitty/Submitty/blob/main/autograder/tests/data/config_files */
 type Course struct {
-	// TODO
+	CourseName string `json:"course_name"` // name of course
+	CourseHomeURL string `json:"course_home_url"` // course home url
+	DefaultHWLateDays int `json:"default_hw_late_days"` /* default number of late days allocated
+	that can be used for hw assignments. */
+	DefaultStudentLateDays int `json:"default_student_late_days"` /* default number
+	of late days students an use for an assignment */
+	UploadMessage string `json:"upload_message"` /* default upload message 
+	when submitting an assignment. */
+	CourseEmail string `json:"course_email"` // course email
 }
