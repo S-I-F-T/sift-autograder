004c0f8ff258280b15653738059583b41cb043aa
huysteve26 <156709309+huysteve26@users.noreply.github.com>
2024-11-14 12:42:58 -0500

    comments for string_array.go

--- pkg/gradables/config/string_array.go
+++ pkg/gradables/config/string_array.go
@@ -2,18 +2,22 @@ package gradables
 
 import "encoding/json"
 
+// Defining a StringArray struct to hold a slice of strings
 type StringArray struct {
 	strings []string
 }
 
+// Constructor function to create a new StringArray with a single string element.
 func StringArrayFromString(s string) *StringArray {
 	return &StringArray{[]string{s}}
 }
 
+// Getter function for slice of strings
 func (sa *StringArray) Strings() []string {
 	return sa.strings
 }
 
+// JSON unmarshaling method to handle both single string and array of strings formats.
 func (sa *StringArray) UnmarshalJSON(data []byte) error {
 	if string(data) == "" || string(data) == "null" {
 		return nil
